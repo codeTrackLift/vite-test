adf0eaa53585ba6b64fb493152290b062ce38d33
Peter Chu <peterc0523@gmail.com>
2023-07-16 09:38:19 -0400

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   base: process.env.NODE_ENV === 'production' ? '/react-vite/' : '/',
 })
